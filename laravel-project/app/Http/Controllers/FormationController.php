3897d891ccfb1b0f024de96c94d7d181dfb7b3de
MariemBenKhlifaa <mariem.benkhlifa@esprit.tn>
2023-10-05 22:20:54 +0100

    formation-crud

--- laravel-project/app/Http/Controllers/FormationController.php
+++ laravel-project/app/Http/Controllers/FormationController.php
@@ -6,40 +6,90 @@ use Illuminate\Http\Request;
 use App\Models\formationModel;
 
 
-
-
 class FormationController extends Controller
 {
-    // Vos méthodes de contrôleur vont ici
+    
+    public function index()
+    {
+        // Votre logique pour la page d'accueil (index) ici
+    }
+
     public function create()
-{
-    return view('formations.create');
-}
+    {
+        return view('formations.create');
+    }
 
     public function store(Request $request)
     {
-        // Validez les données du formulaire
         $data = $request->validate([
             'titre' => 'required|string|max:255',
             'description' => 'required|string',
-            // Ajoutez d'autres règles de validation ici
+            'duree' => 'required|string',
+            'date_debut' => 'required|date',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'prix' => 'required|string'
         ]);
 
-        // Créez une nouvelle formation en utilisant le modèle formationModel
-        $formation = new formationModel($data); // Utilisez le modèle
-         
-        // Enregistrez la formation
+        $image = $request->file('image');
+        $imageName = time() . '.' . $image->getClientOriginalExtension();
+        $image->move(public_path('images'), $imageName);
+
+        $formation = new formationModel($data);
+        $formation->image = $imageName;
         $formation->save();
 
-        // Redirigez vers la page d'affichage de la formation ou ailleurs
-        return redirect()->route('formations.create');
+        return redirect()->route('formations.list')->with('success', 'Formation ajouté avec succès.');
+
     }
-    public function getall(){
+
+    public function getall()
+    {
         $formations = formationModel::all();
         return view('share.home', ['formations' => $formations]);
-
     }
 
+    public function list()
+    {
+        $formations = formationModel::paginate(5); // Récupérez les données de formation paginées
+        return view('formations.list', ['formations' => $formations]); // Passez les données à la vue
+    }
+
+    public function edit(formationModel $formation)
+    {
+        return view('formations.edit', ['formation' => $formation]);
+    }
+
+    public function update(Request $request, formationModel $formation)
+    {
+        $data = $request->validate([
+            'titre' => 'required|string|max:255',
+            'description' => 'required|string',
+            'duree' => 'required|string',
+            'date_debut' => 'required|date',
+            'prix' => 'required|string'
+        ]);
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '.' . $image->getClientOriginalExtension();
+            $image->move(public_path('images'), $imageName);
+            
+            $formation->image = $imageName;
+        }
+    
+        // Mettez à jour les données de la formation
+        $formation->update($data);
+    
+        return redirect()->route('formations.list')->with('success', 'Formation mise à jour avec succès.');
+    }
+    
+
+    public function destroy(formationModel $formation)
+    {
+        // Supprimez la formation
+        $formation->delete();
+
+        return redirect()->route('formations.list')->with('success', 'Formation supprimée avec succès.');
+    }
 
 
 
--- laravel-project/resources/views/formations/create.blade.php
+++ laravel-project/resources/views/formations/create.blade.php
@@ -1,25 +1,82 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Ajouter une formation</title>
-</head>
-<body>
-    <h1>Ajouter une formation</h1>
-    <form method="POST" action="{{ route('formations.store') }}">
-        @csrf
-        <div>
-            <label for="titre">Titre :</label>
-            <input type="text" id="titre" name="titre" required>
+@extends('backoffice.main')
+@section('content')
+<section class="section">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Create Freelancer</h5>
+                    <div>
+                        @if($errors->any())
+                            @foreach($errors->all() as $error)
+                                <div style="color:red">{{$error}}</div>
+                            @endforeach
+                        @endif
+                    </div>
+                    @if(session('success'))
+                        <div id="flash-message" class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                        <script>
+                            // Utilisez setTimeout pour masquer le message flash après 3 secondes (3000 millisecondes)
+                            setTimeout(function() {
+                                var flashMessage = document.getElementById('flash-message');
+                                if (flashMessage) {
+                                    flashMessage.style.display = 'none';
+                                    window.location.href = "{{ route('Freelancer.list') }}"; // Remplacez par votre route souhaitée
+                                }
+                            }, 3000);
+                        </script>
+                    @endif
+                    <!-- General Form Elements -->
+                    <form method="POST" action="{{ route('formations.store') }}" enctype="multipart/form-data">
+                        @csrf
+                        <div class="row mb-3">
+                            <label for="inputText" class="col-sm-2 col-form-label">Titre</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="titre" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="inputText" class="col-sm-2 col-form-label">Description</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="description" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="inputText" class="col-sm-2 col-form-label">Durée</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="duree" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="inputText" class="col-sm-2 col-form-label">Date de début</label>
+                            <div class="col-sm-10">
+                                <input type="date" name="date_debut" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="inputText" class="col-sm-2 col-form-label">Image</label>
+                            <div class="col-sm-10">
+                                <input type="file" name="image" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="inputText" class="col-sm-2 col-form-label">Prix</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="prix" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-sm-2 col-form-label">Bouton de soumission</label>
+                            <div class="col-sm-10">
+                                <button type="submit" class="btn btn-primary">Soumettre le formulaire</button>
+                            </div>
+                        </div>
+                    </form><!-- Fin des éléments généraux du formulaire -->
+                </div>
+            </div>
         </div>
-        <div>
-            <label for="description">Description :</label>
-            <textarea id="description" name="description" required></textarea>
-        </div>
-        <!-- Ajoutez d'autres champs ici -->
-        <div>
-            <button type="submit">Ajouter</button>
-        </div>
-    </form>
-</body>
-</html>
+    </div>
+</section>
+@endsection
