5034ccf8898dc2af4e95fb789234578a9849107b
MariemBenKhlifaa <mariem.benkhlifa@esprit.tn>
2023-10-26 21:01:29 +0100

    update

--- rocketdev/resources/views/share/navbar.blade.php
+++ rocketdev/resources/views/share/navbar.blade.php
@@ -67,8 +67,8 @@
 <header class="header-area">
         <div class="header-navbar">
             <div class="container">
-            <nav class="navbar navbar-expand-lg">
-                    <a class="navbar-brand" href="{{ route('share.home') }}"><img src="{{ asset('assets/images/logo.png') }}" alt="Logo"></a>
+                <nav class="navbar navbar-expand-lg">
+                    <a class="navbar-brand" href="index.html"><img src="assets/images/logo.png" alt="Logo"></a>
 
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="toggler-icon"></span>
@@ -78,29 +78,108 @@
 
                     <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
                         <ul class="navbar-nav ml-auto">
-                            <li class="active"><a href="{{ route('share.home') }}">Home</a></li>
+                            <li><a href="{{ route('share.home') }}">Home</a></li>
                             <li>
                                 <a href="#">Page</a>
 
-
+                                <ul class="sub-menu">
+                                    <li><a href="about.html">About</a></li>
+                                    <li><a href="job-list.html">Job Page</a></li>
+                                    <li><a href="job-details.html">Job Details</a></li>
+                                    <li><a href="resume.html">Resume Page</a></li>
+                                    <li><a href="privacy-policy.html">Privacy Policy</a></li>
+                                    <li><a href="faq.html">FAQ</a></li>
+                                    <li><a href="pricing.html">Pricing Tables</a></li>
+                                    <li><a href="change-password.html">Change Password</a></li>
+                                    <li><a href="notifications.html">Notifications</a></li>
+                                    <li><a href="register.html">Register</a></li>
+                                </ul>
                             </li>
                             <li>
                                 <a href="#">Candidates</a>
 
-
+                                <ul class="sub-menu">
+                                    <li><a href="browse-categories.html">Browse Categories</a></li>
+                                    <li><a href="bookmarked.html">Bookmarked</a></li>
+                                    <li><a href="{{ route('freelancer-resumes.create') }}">Add Resume</a></li>
+                                    <li><a href="manage-resume.html">Manage Resumes</a></li>
+                                    <li><a href="job-alerts.html">Job Alerts</a></li>
+                                </ul>
                             </li>
                             <li>
                                 <a href="#">Employers</a>
 
+                                <ul class="sub-menu">
+                                    <li><a href="post-job.html">Post Job</a></li>
+                                    <li><a href="manage-jobs.html">Manage Jobs</a></li>
+                                    <li><a href="manage-applications.html">Manage Applications</a></li>
+                                    <li><a href="browse-resumes.html">Browse Resumes</a></li>
+                                </ul>
                             </li>
                             <li>
                                 <a href="blog.html">Blog</a>
 
+                                <ul class="sub-menu">
+                                    <li><a href="blog.html">Blog</a></li>
 
+                                </ul>
                             </li>
-                            <li><a href="contact.html">Contact</a></li>
-                            <li><a href="">Log In</a></li>
+                            <style>
+    .login-prompt {
+        display: none;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.6);
+        z-index: 9999;
+    }
 
+    .login-prompt-content {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        padding: 20px;
+        background: #fff;
+        border-radius: 5px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+        text-align: center;
+    }
+
+    .login-prompt-button {
+        margin-top: 10px;
+    }
+</style>
+
+                            @guest
+                            <div class="login-prompt">
+                                <div class="login-prompt-content">
+                                    <p>Please log in to access this content..</p>
+                                    <a href="{{ route('login') }}" class="btn btn-primary login-prompt-button">Log In</a>
+                                    <button class="btn btn-secondary login-prompt-button" id="close-login-prompt">Close</button>
+                                </div>
+                            </div>
+                            @else
+                            <li><a href="{{ route('reclamations.create') }}">Réclamer</a></li>
+                            @endguest
+
+                            @guest
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/login">Login</a>
+                                </li>
+                            @else
+                                <li class="nav-item">
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <a class="nav-link" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault(); this.closest('form').submit();">
+                                            Logout
+                                        </a>
+                                    </form>
+                                </li>
+                            @endguest
                             <li><a class="main-btn" href="post-job.html">Post a job</a></li>
                         </ul> <!-- navbar nav -->
                     </div>
@@ -118,7 +197,6 @@
                         </div>
                     </div> <!-- language -->
                 </nav>
-
             </div> <!-- container -->
         </div> <!-- header navbar -->
 
@@ -140,4 +218,9 @@
             </div> <!-- container -->
         </div> <!-- page banner -->
     </header>
+    <script>
+                                document.getElementById('close-login-prompt').addEventListener('click', function () {
+                                    document.querySelector('.login-prompt').style.display = 'none';
+                                });
+                            </script>
 </body>
--- rocketdev/resources/views/share/navbar1.blade.php
+++ rocketdev/resources/views/share/navbar1.blade.php
@@ -9,30 +9,69 @@
 
                     <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
                         <ul class="navbar-nav ml-auto">
-                            <li class="active"><a href="{{ route('share.home') }}">Home</a></li>
+                            <li class="active"><a href="index.html">Home</a></li>
                             <li>
                                 <a href="#">Page</a>
 
-
+                                <ul class="sub-menu">
+                                    <li><a href="about.html">About</a></li>
+                                    <li><a href="job-list.html">Job Page</a></li>
+                                    <li><a href="job-details.html">Job Details</a></li>
+                                    <li><a href="resume.html">Resume Page</a></li>
+                                    <li><a href="privacy-policy.html">Privacy Policy</a></li>
+                                    <li><a href="faq.html">FAQ</a></li>
+                                    <li><a href="pricing.html">Pricing Tables</a></li>
+                                    <li><a href="change-password.html">Change Password</a></li>
+                                    <li><a href="notifications.html">Notifications</a></li>
+                                    <li><a href="register.html">Register</a></li>
+                                </ul>
                             </li>
                             <li>
                                 <a href="#">Candidates</a>
 
-
+                                <ul class="sub-menu">
+                                    <li><a href="browse-categories.html">Browse Categories</a></li>
+                                    <li><a href="bookmarked.html">Bookmarked</a></li>
+                                    <li><a href="">Add Resume</a></li>
+                                    <li><a href="manage-resume.html">Manage Resumes</a></li>
+                                    <li><a href="job-alerts.html">Job Alerts</a></li>
+                                </ul>
                             </li>
                             <li>
                                 <a href="#">Employers</a>
 
+                                <ul class="sub-menu">
+                                    <li><a href="post-job.html">Post Job</a></li>
+                                    <li><a href="manage-jobs.html">Manage Jobs</a></li>
+                                    <li><a href="manage-applications.html">Manage Applications</a></li>
+                                    <li><a href="browse-resumes.html">Browse Resumes</a></li>
+                                </ul>
                             </li>
                             <li>
                                 <a href="blog.html">Blog</a>
 
-
+                                <ul class="sub-menu">
+                                    <li><a href="blog.html">Blog</a></li>
+                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                </ul>
                             </li>
-                            <li><a href="contact.html">Contact</a></li>
-                            <li><a href="">Log In</a></li>
-
-                            <li><a class="main-btn" href="post-job.html">Post a job</a></li>
+                            <li><a href="contact.html">Contactt</a></li>
+                            @guest
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/login">Login</a>
+                                </li>
+                            @else
+                                <li class="nav-item">
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <a class="nav-link" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault(); this.closest('form').submit();">
+                                            Logout
+                                        </a>
+                                    </form>
+                                </li>
+                            @endguest
+                                                    <li><a class="main-btn" href="post-job.html">Post a job</a></li>
                         </ul> <!-- navbar nav -->
                     </div>
 
@@ -49,3 +88,4 @@
                         </div>
                     </div> <!-- language -->
                 </nav>
+
